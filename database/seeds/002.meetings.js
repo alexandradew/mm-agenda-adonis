18736d7009b73fa0f06ac86d027d74d1ff05e585
Alex Andrade <alexandradew@protonmail.com>
2021-10-31 16:20:00 -0300

    Update 002.meetings.js

--- database/seeds/002.meetings.js
+++ database/seeds/002.meetings.js
@@ -6,16 +6,16 @@ class Seeds {
   async run() {
     if (await Meeting.findBy('id', 1) === null) {
       let a = new Meeting()
-      a.name = 'Daily'
-      a.start_time = '8:00'
-      a.finish_time = '9:00'
+      a.name = 'Daily scrum'
+      a.start_time = '08:00'
+      a.finish_time = '09:00'
       a.date = '2021-11-25'
       await a.save()
     }
 
     if (await Meeting.findBy('id', 2) === null) {
       let a = new Meeting()
-      a.name = 'Daily 2'
+      a.name = 'Alinhamento com cliente'
       a.start_time = '10:00'
       a.finish_time = '12:00'
       a.date = '2021-10-12'
